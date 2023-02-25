4aaa4af278b1712c336931420f4a14793dabea12
shashank singh chandel <shashakchandel@gmail.com>
2023-02-25 22:21:12 +0530

    v1 commit

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+export const App = () => <h1>My React and TypeScript setup without CRA!</h1>;
---
+++ webpack.config.ts
@@ -0,0 +1,54 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?＄/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+              'es2015',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'build'),
+    compress: true,
+    port: 4000,
+  },
+};
+
+export default config;
